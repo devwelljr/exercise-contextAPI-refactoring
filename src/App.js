33142252f96c4a11c72d500f4e62bfe1d51578bb
Wellington <junior.wel02@exemplo.br>
2021-08-12 13:30:54 -0300

    exercise one

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import './App.css';
 import Cars from './Cars';
+import MyContext from './MyContext';
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      redCar: false,
+      blueCar: false,
+      yellowCar: false,
+    }
+    this.moveCar = this.moveCar.bind(this);
+  }
+
+  moveCar(state) {
+    this.setState((prevState) => ({
+      ...prevState,
+      [Object.keys(state)[0]]: !Object.values(state)[0],
+    }))
+  }
+
+  render() {
+    const contextValue = {
+      ...this.state,
+      moveCar: this.moveCar,
+    }
+
+    return (
+      <MyContext.Provider
+        value={contextValue}
+      >
+        <Cars />
+      </MyContext.Provider>
+    );
+  }
 }
 
 export default App;
